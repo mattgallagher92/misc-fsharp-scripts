386bdcc8f277df6d1b79ba6dd1b76badfa7710ae
Matt <mjg1992@hotmail.co.uk>
2022-02-08 21:59:41 +0000

    Add DartTarget

---
+++ DartTarget.fsx
@@ -0,0 +1,31 @@
+﻿// Inspired by https://nrich.maths.org/12261
+
+type ListBuilder() =
+    member this.Bind(list, f) = list |> List.collect f
+    member this.Return(x) = [x]
+
+let list = ListBuilder()
+
+let allZones = [ 1; 1; 1; 2; 2; 2; 3; 3; 3; 4; 4; 5; 5 ]
+
+let allStrictlyOrderedQuadruples = list {
+    let! i = [ 0 .. 11 ]
+    let! j = [ (i + 1) .. 11 ]
+    let! k = [ (j + 1) .. 11 ]
+    let! l = [ (k + 1) .. 11 ]
+    return i, j, k, l
+}
+
+// Expect 12C4 = 495
+List.length allStrictlyOrderedQuadruples
+
+let possibleGames =
+    allStrictlyOrderedQuadruples
+    |> List.map (fun (i, j, k, l) -> let itemN n = List.item n allZones in itemN i, itemN j, itemN k, itemN l)
+    |> List.distinct
+
+let score (i, j, k, l) = i + j + k + l
+
+possibleGames
+|> List.filter (fun g -> let s = score g in s = 11 || s = 12 || s = 13)
+|> List.groupBy score
